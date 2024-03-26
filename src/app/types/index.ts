6e864e0d2d120c441abab8f356f5fcb30919eaf2
Minthantmg <minthant180@gmail.com>
2024-03-26 18:34:22 +0900

    export components

---
+++ src/app/types/index.ts
@@ -0,0 +1,9 @@
+
+export interface ButtonProps{
+    label: string,
+    iconURL?: string,
+    backgroundColor?: string,
+    textColor?: string,
+    borderColor?: string,
+    fullWidth?: string,
+}
