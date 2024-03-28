ef1dc02585a061fc787eb2e409719488ba08b1f6
Minthantmg <minthant180@gmail.com>
2024-03-28 18:57:47 +0900

    added data

---
+++ constants/index.ts
@@ -0,0 +1,77 @@
+import iphone from '../public/iph.jpg'
+
+export const Products = [
+    {
+        id: 1,
+        title: "IPhone 15",
+        price: "999",
+        brand: "IPhone",
+        image: iphone,
+        category: "phone"
+    },
+    {
+        id: 2,
+        title: "IPhone 15 pro",
+        price: "1199",
+        brand: "IPhone",
+        image: iphone,
+        category: "phone"
+    },
+    {
+        id: 3,
+        title: "IPhone 15 pro max",
+        price: "1399",
+        brand: "IPhone",
+        image: iphone,
+        category: "phone"
+    },
+    {
+        id: 4,
+        title: "IPhone 15 double pro max",
+        price: "1499",
+        brand: "IPhone",
+        image: iphone,
+        category: "phone"
+    },
+    {
+        id: 5,
+        title: "IPhone 15",
+        price: "999",
+        brand: "IPhone",
+        image: iphone,
+        category: "phone"
+    },
+    {
+        id: 6,
+        title: "IPhone 15 pro max",
+        price: "1399",
+        brand: "IPhone",
+        image: iphone,
+        category: "phone"
+    },
+    {
+        id: 7,
+        title: "IPhone 15 double pro max",
+        price: "1499",
+        brand: "IPhone",
+        image: iphone,
+        category: "phone"
+    },
+    {
+        id: 8,
+        title: "Samsung s24 ultra",
+        price: "1499",
+        brand: "Samsung",
+        image: iphone,
+        category: "phone"
+    },
+    {
+        id: 9,
+        title: "Samsung TV",
+        price: "2499",
+        brand: "Samsung",
+        image: iphone,
+        category: "TV"
+    },
+]
+
