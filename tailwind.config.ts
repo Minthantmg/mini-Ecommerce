476bcbf0902a53ba25ae248a0cef4e4b83d5f7c4
Minthantmg <minthant180@gmail.com>
2024-04-01 16:50:01 +0900

    added config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,17 +10,6 @@ const config = {
 	],
   prefix: "",
   theme: {
-    fontSize: {
-      xs: ['12px', '16px'],
-      sm: ['14px', '20px'],
-      base: ['16px', '19.5px'],
-      lg: ['18px', '21.94px'],
-      xl: ['20px', '24.38px'],
-      '2xl': ['24px', '29.26px'],
-      '3xl': ['28px', '50px'],
-      '4xl': ['48px', '58px'],
-      '8xl': ['96px', '106px']
-    },
     screens: {
       "wide": "1440px",
       "tablet_wide": "768px",
@@ -32,9 +21,6 @@ const config = {
       screens: {
         "2xl": "1400px",
       },
-    },
-    colors:{
-      pink : "#EDA3B5"
     },
     extend: {
       colors: {
@@ -93,7 +79,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"),require("daisyui")],
 
 } satisfies Config
 
