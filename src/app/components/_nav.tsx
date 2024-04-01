6ca8390d23ac8a60b904bb7d9b669d5d66a87873
Minthantmg <minthant180@gmail.com>
2024-04-01 16:51:58 +0900

    updated nav ui

--- src/app/components/_nav.tsx
+++ src/app/components/_nav.tsx
@@ -2,6 +2,19 @@
 import React, {useEffect} from 'react';
 import Cart from '../../../public/cart'
 import {useRouter} from "next/navigation";
+import {Button} from "@/components/ui/button"
+import {Input} from "@/components/ui/input"
+import {Label} from "@/components/ui/label"
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
 
 const _Nav = () => {
     const router = useRouter()
@@ -22,30 +35,38 @@ const _Nav = () => {
 
     return (
         <div>
-            <div className="flex justify-between items-center fixed bg-white w-full px-32 border border-b-gray-100">
-                <div className="rounded-full bg-red-500 text-white text-xs mx-2 my-2 px-1.5 py-7 font-bold cursor-pointer"
-                     onClick={gotoHome}>
+            <div className="flex justify-between items-center fixed z-10 bg-white w-full px-32 shadow-sm">
+                <div
+                    className="rounded-full bg-red-500 text-white text-xs mx-2 my-2 px-1.5 py-7 font-bold cursor-pointer"
+                    onClick={gotoHome}>
                     ミニマート
                 </div>
                 <div className="flex gap-10">
-                    <div className="font-mono cursor-pointer" onClick={gotoCategories}>
+                    <div className="font-mono cursor-pointer hover:underline" onClick={gotoCategories}>
                         CATEGORIES
                     </div>
-                    <div className="font-mono cursor-pointer">
+                    <div className="font-mono cursor-pointer hover:underline">
                         PRODUCT PAGE
                     </div>
                     <div>
-                        <span>0</span>
-                        <div className="absolute">
-                            <Cart />
-                        </div>
+                        <Sheet>
+                            <SheetTrigger>
+                                <div className="indicator">
+                                    <span className="indicator-item badge badge-primary text-white">0</span>
+                                    <Cart/>
+                                </div>
+                            </SheetTrigger>
+                            <SheetContent>
+                                <SheetHeader>
+                                    <SheetTitle>Are you absolutely sure?</SheetTitle>
+                                    <SheetDescription>
+                                        This action cannot be undone. This will permanently delete your account
+                                        and remove your data from our servers.
+                                    </SheetDescription>
+                                </SheetHeader>
+                            </SheetContent>
+                        </Sheet>
                     </div>
-                    {/*<div className="avatar indicator">*/}
-                    {/*    <span className="indicator-item badge badge-secondary">0</span>*/}
-                    {/*    <div className="cursor-pointer">*/}
-                    {/*        <Cart/>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
                 </div>
             </div>
         </div>
