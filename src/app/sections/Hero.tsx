ba218340bb1c0c367874b45b20edf83c8bcd3a5e
Minthantmg <minthant180@gmail.com>
2024-04-01 16:51:12 +0900

    updated ui

--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -2,33 +2,89 @@ import React from 'react';
 import {Products} from "../../../constants";
 import CustomCard from "@/app/components/CustomCard";
 import Carousel from "@/app/components/CustomCarousel";
-import Image from "next/image";
-import women from "../../../public/women_collection.png"
 
 const Hero = () => {
     return (
         <div className="wide:pt-32">
             <div className="flex">
-                <div className="w-1/2 rounded-lg">
-                    <Image src={women} alt="collection" width={0} height={0}/>
+                <div className="w-1/2 rounded-lg h-[500px] bg-pink-200 flex justify-start items-end">
+                    <div className="ml-4 mb-6">
+                        <div className="text-white font-bold text-3xl">
+                            Spring 2024
+                        </div>
+                    </div>
                 </div>
-                <div className="w-1/2">
-                    hello
+                <div className="w-1/2 rounded-lg h-[500px]">
+                    <div className="h-[240px] flex gap-4">
+                        <div className="w-1/2 bg-gray-300 rounded-lg ml-4 text-white flex items-end">
+                            <div className="ml-4 mb-4 text-3xl font-bold">
+                                Health
+                            </div>
+                        </div>
+                        <div className="w-1/2 bg-purple-500 rounded-lg mr-4 flex items-end">
+                            <div className="m-4 text-white text-3xl font-bold">
+                                New
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bg-pink-500 rounded-lg h-[240px] mt-4 ml-4 mr-4 flex items-end">
+                        <div className="text-white m-6 text-3xl font-bold">
+                            New
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="font-bold text-xl">
+
+            <div className="font-bold text-xl pt-10">
                 Product we are proud of
             </div>
             <div
                 className="grid grid-cols-2 wide:grid-cols-4 tablet_wide:grid-cols-2 small_phone_wide:grid-cols-1 gap-4 wide:pt-8">
                 {Products.slice(0, 8).map((product) => (
-                    <CustomCard key={product.id} title={product.title} price={product.price} image={product.image}/>
+                    <CustomCard key={product.id} title={product.title} price={product.price} image={product.image} containerStyles="border-2"/>
                 ))}
             </div>
+
+            <div className="flex mt-20">
+                <div className="w-1/2 bg-gray-200 flex flex-col justify-center items-center">
+                    <div className="ml-32">
+                        <div className="font-bold text-3xl">
+                            Creative harmonious living
+                        </div>
+                        <div className="mr-32 mt-2 text-black">
+                            RAOUF Products are all made to standard sizes so that you can mix and match them freely.
+                        </div>
+                        <div className="mt-4 w-fit bg-black text-white">
+                            <button className="px-4 py-2">Shop Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-1/2 bg-gray-400 h-96">
+                </div>
+            </div>
+
             <div className="font-bold text-xl pt-16">
                 Trending Now
             </div>
             <Carousel/>
+
+            <div className="flex mt-20 pb-20">
+                <div className="w-1/2 bg-gray-400 h-96">
+                </div>
+                <div className="w-1/2 bg-gray-200 flex flex-col justify-center items-center">
+                    <div className="ml-32">
+                        <div className="font-bold text-3xl">
+                            Comfortable & Elegante Living
+                        </div>
+                        <div className="mr-32 mt-2 text-black">
+                            RAOUF Products are all made to standard sizes so that you can mix and match them freely.
+                        </div>
+                        <div className="mt-4 w-fit bg-black text-white">
+                            <button className="px-4 py-2">Shop Now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
