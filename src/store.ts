57efaa794b649ec3d9967e909e534e4a5fe6f71b
Minthantmg <minthant180@gmail.com>
2024-04-03 18:25:08 +0900

    added cartData reducer

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
 import cartReducer from "./app/features/CartSlice"
+import cartDataReducer from './app/features/CartDataSlice'
 
 export const store = configureStore({
     reducer:{
         cart : cartReducer,
+        cartData : cartDataReducer
     },
 })
