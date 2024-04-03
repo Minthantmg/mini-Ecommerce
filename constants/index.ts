1bf485c5af5a0f1a2368ceaa4843158fa152a06d
Minthantmg <minthant180@gmail.com>
2024-04-03 18:27:13 +0900

    updated cartData

--- constants/index.ts
+++ constants/index.ts
@@ -7,7 +7,12 @@ export const Products = [
         price: "999",
         brand: "Apple",
         image: iphone,
-        category: "phone"
+        category: "phone",
+        quantity: 1,
+        description: "iPhone 15 has a 6.1-inch Super Retina XDR OLED display with a resolution of 2556 x 1179 pixels and a refresh rate of 60 Hz. It has a 48 MP main camera, 2x telephoto lens, and focus-pocus portraits. The iPhone 15 also has an A16 Bionic chip, USB-C connector, and safety features " +
+            "like Roadside Assistance via satellite, Emergency SOS via satellite, and Crash Detection.",
+        weight : "120kg",
+        size : "480x480"
     },
     {
         id: 2,
@@ -15,7 +20,12 @@ export const Products = [
         price: "1199",
         brand: "Apple",
         image: iphone,
-        category: "phone"
+        category: "phone",
+        quantity: 1,
+        description: "iPhone 15 has a 6.1-inch Super Retina XDR OLED display with a resolution of 2556 x 1179 pixels and a refresh rate of 60 Hz. It has a 48 MP main camera, 2x telephoto lens, and focus-pocus portraits. The iPhone 15 also has an A16 Bionic chip, USB-C connector, and safety features " +
+            "like Roadside Assistance via satellite, Emergency SOS via satellite, and Crash Detection.",
+        weight : "120kg",
+        size : "480x480"
     },
     {
         id: 3,
@@ -23,7 +33,12 @@ export const Products = [
         price: "1399",
         brand: "Apple",
         image: iphone,
-        category: "phone"
+        category: "phone",
+        quantity: 1,
+        description: "iPhone 15 has a 6.1-inch Super Retina XDR OLED display with a resolution of 2556 x 1179 pixels and a refresh rate of 60 Hz. It has a 48 MP main camera, 2x telephoto lens, and focus-pocus portraits. The iPhone 15 also has an A16 Bionic chip, USB-C connector, and safety features " +
+            "like Roadside Assistance via satellite, Emergency SOS via satellite, and Crash Detection.",
+        weight : "120kg",
+        size : "480x480"
     },
     {
         id: 4,
@@ -31,7 +46,12 @@ export const Products = [
         price: "1499",
         brand: "Apple",
         image: iphone,
-        category: "phone"
+        category: "phone",
+        quantity: 1,
+        description: "iPhone 15 has a 6.1-inch Super Retina XDR OLED display with a resolution of 2556 x 1179 pixels and a refresh rate of 60 Hz. It has a 48 MP main camera, 2x telephoto lens, and focus-pocus portraits. The iPhone 15 also has an A16 Bionic chip, USB-C connector, and safety features " +
+            "like Roadside Assistance via satellite, Emergency SOS via satellite, and Crash Detection.",
+        weight : "120kg",
+        size : "480x480"
     },
     {
         id: 5,
@@ -39,7 +59,12 @@ export const Products = [
         price: "999",
         brand: "Apple",
         image: iphone,
-        category: "phone"
+        category: "phone",
+        quantity: 1,
+        description: "iPhone 15 has a 6.1-inch Super Retina XDR OLED display with a resolution of 2556 x 1179 pixels and a refresh rate of 60 Hz. It has a 48 MP main camera, 2x telephoto lens, and focus-pocus portraits. The iPhone 15 also has an A16 Bionic chip, USB-C connector, and safety features " +
+            "like Roadside Assistance via satellite, Emergency SOS via satellite, and Crash Detection.",
+        weight : "120kg",
+        size : "480x480"
     },
     {
         id: 6,
@@ -47,7 +72,12 @@ export const Products = [
         price: "1399",
         brand: "IPhone",
         image: iphone,
-        category: "phone"
+        category: "phone",
+        quantity: 1,
+        description: "iPhone 15 has a 6.1-inch Super Retina XDR OLED display with a resolution of 2556 x 1179 pixels and a refresh rate of 60 Hz. It has a 48 MP main camera, 2x telephoto lens, and focus-pocus portraits. The iPhone 15 also has an A16 Bionic chip, USB-C connector, and safety features " +
+            "like Roadside Assistance via satellite, Emergency SOS via satellite, and Crash Detection.",
+        weight : "120kg",
+        size : "480x480"
     },
     {
         id: 7,
@@ -55,7 +85,12 @@ export const Products = [
         price: "1499",
         brand: "IPhone",
         image: iphone,
-        category: "phone"
+        category: "phone",
+        quantity: 1,
+        description: "iPhone 15 has a 6.1-inch Super Retina XDR OLED display with a resolution of 2556 x 1179 pixels and a refresh rate of 60 Hz. It has a 48 MP main camera, 2x telephoto lens, and focus-pocus portraits. The iPhone 15 also has an A16 Bionic chip, USB-C connector, and safety features " +
+            "like Roadside Assistance via satellite, Emergency SOS via satellite, and Crash Detection.",
+        weight : "120kg",
+        size : "480x480"
     },
     {
         id: 8,
@@ -63,7 +98,12 @@ export const Products = [
         price: "1499",
         brand: "Samsung",
         image: iphone,
-        category: "phone"
+        category: "phone",
+        quantity: 1,
+        description: "iPhone 15 has a 6.1-inch Super Retina XDR OLED display with a resolution of 2556 x 1179 pixels and a refresh rate of 60 Hz. It has a 48 MP main camera, 2x telephoto lens, and focus-pocus portraits. The iPhone 15 also has an A16 Bionic chip, USB-C connector, and safety features " +
+            "like Roadside Assistance via satellite, Emergency SOS via satellite, and Crash Detection.",
+        weight : "120kg",
+        size : "480x480"
     },
     {
         id: 9,
@@ -71,7 +111,12 @@ export const Products = [
         price: "2499",
         brand: "Samsung",
         image: iphone,
-        category: "TV"
+        category: "TV",
+        quantity: 1,
+        description: "iPhone 15 has a 6.1-inch Super Retina XDR OLED display with a resolution of 2556 x 1179 pixels and a refresh rate of 60 Hz. It has a 48 MP main camera, 2x telephoto lens, and focus-pocus portraits. The iPhone 15 also has an A16 Bionic chip, USB-C connector, and safety features " +
+            "like Roadside Assistance via satellite, Emergency SOS via satellite, and Crash Detection.",
+        weight : "120kg",
+        size : "480x480"
     },
     {
         id: 10,
@@ -79,7 +124,12 @@ export const Products = [
         price: "3499",
         brand: "Apple",
         image: iphone,
-        category: "TV"
+        category: "TV",
+        quantity: 1,
+        description: "iPhone 15 has a 6.1-inch Super Retina XDR OLED display with a resolution of 2556 x 1179 pixels and a refresh rate of 60 Hz. It has a 48 MP main camera, 2x telephoto lens, and focus-pocus portraits. The iPhone 15 also has an A16 Bionic chip, USB-C connector, and safety features " +
+            "like Roadside Assistance via satellite, Emergency SOS via satellite, and Crash Detection.",
+        weight : "120kg",
+        size : "480x480"
     },
     {
         id: 11,
@@ -87,7 +137,12 @@ export const Products = [
         price: "4499",
         brand: "Apple",
         image: iphone,
-        category: "TV"
+        category: "TV",
+        quantity: 1,
+        description: "iPhone 15 has a 6.1-inch Super Retina XDR OLED display with a resolution of 2556 x 1179 pixels and a refresh rate of 60 Hz. It has a 48 MP main camera, 2x telephoto lens, and focus-pocus portraits. The iPhone 15 also has an A16 Bionic chip, USB-C connector, and safety features " +
+            "like Roadside Assistance via satellite, Emergency SOS via satellite, and Crash Detection.",
+        weight : "120kg",
+        size : "480x480"
     },
     {
         id: 12,
@@ -95,22 +150,36 @@ export const Products = [
         price: "1499",
         brand: "Samsung",
         image: iphone,
-        category: "TV"
+        category: "TV",
+        quantity: 1,
+        description: "iPhone 15 has a 6.1-inch Super Retina XDR OLED display with a resolution of 2556 x 1179 pixels and a refresh rate of 60 Hz. It has a 48 MP main camera, 2x telephoto lens, and focus-pocus portraits. The iPhone 15 also has an A16 Bionic chip, USB-C connector, and safety features " +
+            "like Roadside Assistance via satellite, Emergency SOS via satellite, and Crash Detection.",
+        weight : "120kg",
+        size : "480x480"
     },
 ]
 
 export const filterItems = [
     {
         name: "All",
-        category : "All"
+        category: "All"
     },
     {
         name: "phone",
-        category : "phone"
+        category: "phone"
     },
     {
         name: "TV",
-        category : "TV"
+        category: "TV"
     },
+]
+
+export const cartItems = [{
+    id: 1,
+    title : "test",
+    price : "1",
+    quantity : "1",
+    image : iphone
+},
 ]
 
