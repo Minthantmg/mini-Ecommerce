edd26fb007089d764b9c4d5f2139d8004e6f2297
Minthantmg <minthant180@gmail.com>
2024-04-10 18:38:12 +0900

    changed card ui

--- src/app/components/CustomCard.tsx
+++ src/app/components/CustomCard.tsx
@@ -3,13 +3,13 @@ import {CustomCardProps} from "@/app/types";
 
 const CustomCard = ({ containerStyles, textStyles, title , price,image }: CustomCardProps) => (
     <div
-        className={`border border-gray-300 hover:border-black bg-white cursor-pointer ${containerStyles}`}
+        className={`border border-gray-300 hover:border-black bg-white cursor-pointer h-[420px] ${containerStyles}`}
     >
         <div className="flex justify-center items-center">
-            <Image src={image} alt={title} className="w-52 h-52" width={0} height={0}/>
+            <img src={image} alt={title} className="w-72 h-72 p-10" width={0} height={0}/>
         </div>
         <div className="flex flex-col mt-6 mb-4">
-            <span className={`font-mono ml-4 ${textStyles}`}>{title}</span>
+            <span className={`font-mono ml-4 line-clamp-2 ${textStyles}`}>{title}</span>
             <span className={`font-bold font-mono ml-4 mt-1 ${textStyles}`}>{price}$</span>
         </div>
     </div>
