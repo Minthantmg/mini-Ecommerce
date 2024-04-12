c2ce8994a673507e171e9be0d73b9a44e2134026
Minthantmg <minthant180@gmail.com>
2024-04-12 16:09:05 +0900

    added toast message

--- src/app/features/CartDataSlice.ts
+++ src/app/features/CartDataSlice.ts
@@ -1,5 +1,7 @@
 import {createSlice} from "@reduxjs/toolkit";
 import {cartItems, Products} from "../../../constants";
+import {toast} from "@/components/ui/use-toast";
+import {className} from "postcss-selector-parser";
 
 const initialState = {
     cartData: cartItems,
@@ -15,13 +17,46 @@ const cartDataSlice = createSlice({
             // @ts-ignore
             state.cartData.push({...action.payload});
         },
-        clearCart:(state)=>{
+        clearCart: (state) => {
             state.cartData = []
+            toast({
+                title: "Checkout successfully",
+            });
+        },
+        removeItem: (state, action) => {
+            const itemId = action.payload
+            state.cartData = state.cartData.filter((item) => item.id !== itemId)
+        },
+        increase: (state, {payload}) => {
+            const cartData = state.cartData.find((item) =>
+                item.id === payload)
+            // @ts-ignore
+            cartData.quantity += 1
+        },
+        decrease: (state, {payload}) => {
+            const cartData = state.cartData.find((item) =>
+                item.id === payload)
+            // @ts-ignore
+            cartData.quantity -= 1
+        },
+        calculateTotal: (state) => {
+            let amount = 0
+            let total = 0
+            state.cartData.forEach((item) => {
+                // @ts-ignore
+                amount += item.quantity
+                // @ts-ignore
+                total += item.quantity * item.price
+            })
+            state.total = total
         }
     },
 });
 
 
-export const  {clearCart} = cartDataSlice.actions
-export const {addItemToCart} = cartDataSlice.actions;
+export const {
+    addItemToCart, clearCart,
+    removeItem,increase,
+    decrease,calculateTotal
+} = cartDataSlice.actions;
 export default cartDataSlice.reducer
