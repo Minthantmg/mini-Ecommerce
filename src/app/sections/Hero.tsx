859da6e3b83debf362cda8d0de68ff0f1342946c
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-04-16 21:27:44 +0630

    handle mobile view

--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -31,19 +31,19 @@ const Hero = () => {
             {isError && <ErrorGif />}
             {isSuccess && (
                 <>
-                    <div className="sm:pt-32">
-                        <div className="flex">
-                            <div className="w-1/2 rounded-lg h-[500px] bg-pink-200 flex justify-start items-end">
+                    <div className="sm:pt-32 pt-28">
+                        <div className="sm:flex">
+                            <div className="sm:w-1/2 rounded-lg h-[300px] sm:h-[500px] bg-pink-200 flex justify-start items-end">
                                 <div className="ml-4 mb-6">
                                     <div className="text-white font-bold text-3xl">
                                         Spring 2024
                                     </div>
                                 </div>
                             </div>
-                            <div className="w-1/2 rounded-lg h-[500px]">
+                            <div className="sm:w-1/2 rounded-lg h-[500px] mt-4 sm:mt-0">
                                 <div className="h-[240px] flex gap-4">
-                                    <div className="w-1/2 bg-gray-300 rounded-lg ml-4 text-white flex items-end">
-                                        <div className="ml-4 mb-4 text-3xl font-bold">
+                                    <div className="w-1/2 bg-gray-300 rounded-lg sm:ml-4 text-white flex items-end">
+                                        <div className="sm:ml-4 sm:mb-4 text-3xl font-bold">
                                             Health
                                         </div>
                                     </div>
@@ -53,9 +53,9 @@ const Hero = () => {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="group hover:opacity-90 rounded-lg h-[240px] mt-4 ml-4 flex items-end">
+                                <div className="group hover:opacity-90 rounded-lg h-[240px] mt-4 sm:ml-4 flex items-end">
                                     <Image src={image_four} alt="header_image_four"
-                                           className="h-60 absolute rounded-lg w-[560px]"/>
+                                           className="h-60 absolute rounded-lg sm:w-[560px] w-[335px]"/>
                                     <div
                                         className="relative text-white m-6 text-3xl font-bold group-hover:text-opacity-70">
                                         New
@@ -68,7 +68,7 @@ const Hero = () => {
                             Product we are proud of
                         </div>
                         <div
-                            className="grid grid-cols-2 sm:grid-cols-4 gap-4 sm:pt-4">
+                            className="grid grid-cols-1 sm:grid-cols-4 gap-4 pt-4 pb-4 sm:pb-0">
                             {products.map((item: any) => (
                                 <Link href={`./sections/product_page/${item.id}`} key={item.id}>
                                     <CustomCard title={item.title} price={item.price}
@@ -78,13 +78,13 @@ const Hero = () => {
                             ))}
                         </div>
 
-                        <div className="flex mt-20">
-                            <div className="w-1/2 bg-gray-200 flex flex-col justify-center items-center">
-                                <div className="ml-32">
+                        <div className="sm:flex sm:mt-20 mt-8 pb-4 sm:pb-0">
+                            <div className="sm:w-1/2 bg-gray-200 flex flex-col justify-center items-center">
+                                <div className="sm:ml-32 p-10 sm:p-0">
                                     <div className="font-bold text-3xl">
                                         Creative harmonious living
                                     </div>
-                                    <div className="mr-32 mt-2 text-black">
+                                    <div className="sm:mr-32 mt-2 text-black">
                                         RAOUF Products are all made to standard sizes so that you can mix and match them
                                         freely.
                                     </div>
@@ -93,7 +93,7 @@ const Hero = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="w-1/2 bg-gray-400 h-96">
+                            <div className="sm:w-1/2 bg-gray-400 sm:h-96 h-72">
                             </div>
                         </div>
 
@@ -102,15 +102,15 @@ const Hero = () => {
                         </div>
                         <Carousel/>
 
-                        <div className="flex pt-10 pb-20">
-                            <div className="w-1/2 bg-gray-400 h-96">
+                        <div className="sm:flex sm:pt-10 sm:pb-20 pt-4 pb-4">
+                            <div className="sm:w-1/2 bg-gray-400 sm:h-96 h-72">
                             </div>
-                            <div className="w-1/2 bg-gray-200 flex flex-col justify-center items-center">
-                                <div className="ml-32">
+                            <div className="sm:w-1/2 bg-gray-200 flex flex-col justify-center items-center">
+                                <div className="sm:ml-32 p-10 sm:p-0">
                                     <div className="font-bold text-3xl">
                                         Comfortable & Elegante Living
                                     </div>
-                                    <div className="mr-32 mt-2 text-black">
+                                    <div className="sm:mr-32 mt-2 text-black">
                                         RAOUF Products are all made to standard sizes so that you can mix and match them
                                         freely.
                                     </div>
