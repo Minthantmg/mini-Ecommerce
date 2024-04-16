f936d8bcdc1a5cc5bad0ee33034fb66ab913272a
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-04-16 14:30:33 +0630

    create loading ui

---
+++ src/app/components/loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from "next/image";
+// @ts-ignore
+import loading from '../../../public/loading.gif'
+const Loading = () => {
+    return (
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+            <Image src={loading} alt="loading" />
+        </div>
+    );
+};
+
+export default Loading;
