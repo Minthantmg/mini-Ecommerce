c94aba7cd36e9b1081aeed25606012bc0e0eb1b2
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-04-16 14:30:16 +0630

    error page code

---
+++ src/app/components/errorGif.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Image from "next/image";
+import notfound from '../../../public/not_found.svg'
+
+const ErrorGif = () => {
+    return (
+        <>
+            <div className="w-full h-screen flex flex-col justify-center items-center">
+                <Image src={notfound} alt="notfound" className="w-screen h-80"></Image>
+                <div className="mt-10 text-3xl">
+                    404
+                </div>
+                <div className="mt-4">
+                    Sorry, we coundn't find this page.
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ErrorGif;
