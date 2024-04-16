e2c75664edbadab5e88f1f652e2d0e23b19f6136
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-04-16 21:27:41 +0630

    handle mobile view

--- src/app/components/CustomCarousel.tsx
+++ src/app/components/CustomCarousel.tsx
@@ -17,17 +17,18 @@ const CustomCarousel = () => {
     return (
         <>
             {isSuccess && (
-                <div className="sm:pb-6">
+                <div className="sm:pb-6 px-5 sm:px-0">
                     <Carousel>
                         <CarouselContent>
                             {products.map((product: any) => (
-                                <CarouselItem className="sm:basis-1/4 basis-1/2 my-4" key={product.id}>
+                                <CarouselItem className="sm:basis-1/4 my-4" key={product.id}>
                                     <Link href={`../../sections/product_page/${product.id}`}>
                                         <div
-                                            className={`border-2 border-gray-300 hover:border-black bg-white cursor-pointer w-66 h-[360px]`}
+                                            className={`border-2 border-gray-300 hover:border-black bg-white cursor-pointer sm:w-66 
+                                            h-[360px]`}
                                         >
                                             <div className="flex justify-center items-center">
-                                                <img src={product.image} alt={product.title} className="w-52 h-52 p-4"/>
+                                                <img src={product.image} alt={product.title} className="w-52 h-52 sm:p-4 p-2"/>
                                             </div>
                                             <div className="flex flex-col mt-6 mb-4">
                                                 <span className={`font-mono ml-4 line-clamp-3`}>{product.title}</span>
