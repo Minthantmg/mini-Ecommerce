b483fb29b73d535b77fbc37619f75d66fc1aa6a4
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-04-18 22:21:36 +0630

    nav mobile ui test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 'use client'
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Hero} from "@/app/sections";
 import {useDispatch, useSelector} from "react-redux";
 import {calculateTotal} from "@/app/features/CartDataSlice";
 
 const Page = () => {
+    console.log("hello")
     return (
         <>
             <div className="container px-8 sm:px-32">
-                <Hero />
+                <Hero/>
             </div>
         </>
     );
