049ca7c92ed282ae3ace5ec1013b596f694daaf4
Minthantmg <minthant180@gmail.com>
2024-04-22 14:16:01 +0900

    removed imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 'use client'
 import React, {useEffect, useState} from 'react';
 import {Hero} from "@/app/sections";
-import {useDispatch, useSelector} from "react-redux";
-import {calculateTotal} from "@/app/features/CartDataSlice";
+import Image from "next/image";
+import close from "../../public/close.svg";
+import Nav from "../app/components/_nav"
 
 const Page = () => {
     return (
         <>
-            <div className="container px-8 sm:px-32">
-                <Hero/>
-            </div>
+            <>
+                <div className="relative container px-8 sm:px-32">
+                    <Hero/>
+                </div>
+            </>
         </>
     );
 };
